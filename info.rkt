e52cee00555c4bca7fdbc6e1cad9362c21b68ecd
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-07-13 11:36:17 -0400

    Add dependencies.

--- info.rkt
+++ info.rkt
@@ -1,3 +1,9 @@
 #lang setup/infotab
 
 (define collection 'multi)
+(define deps '("srfi-lite-lib"
+               "base"
+               "in-new-directory"))
+(define build-deps '("compatibility-lib"
+                     "rackunit-gui"
+                     "rackunit-lib"))
